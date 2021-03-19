e405c7d8ed2e2f81027599f48e35d59f59a2617e
gapry <gapry@protonmail.com>
2021-03-19 22:05:48 +0800

    unittest: update

---
+++ scripts/unittest.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+declare -r proj=GNet
+declare -r dir_build=build
+declare -r dir_scripts=scripts
+declare -r unnittest=${proj}_unittest.out
+
+rm -rf $dir_build; 
+
+bash -x $dir_scripts/fmt.sh
+
+mkdir $dir_build; 
+
+cmake -B $dir_build .
+
+make -C $dir_build; 
+
+$dir_build/$unnittest
--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,29 @@
 #include <fmt/core.h>
 
+#include "gnet/string.h"
+#include "gnet/vector.h"
+
 #include "gstring.h"
+#include "gvector.h"
+
 #include "noncopyable.h"
 #include "thread/thread.h"
-#include "vector.h"
 
 auto test_noncopyable(void) -> void {
   gnet::noncopyable obj;
 }
 
-auto test_vector(void) -> void {
-  gnet::vector<int> vec;
-  vec.m_vec.push_back(10);
-  fmt::print("[log] {}\n", vec.m_vec[0]);
+auto test_gnet_vector(void) -> void {
+  gnet::beta::vector<int> v1;
+  gnet::vector<int> v2;
+  v2.push_back(11);
+  fmt::print("{}\n", v2[0]);
 }
 
-auto test_string(void) -> void {
-  gnet::string msg;
-  msg.m_string = "I go to school by bus.";
-  fmt::print("[log] {}\n", msg.m_string);
+auto test_gnet_string(void) -> void {
+  gnet::beta::string msg1;
+  gnet::string msg2 = "I go to school by bus";
+  fmt::print("{}\n", msg2);
 }
 
 auto test_gnet_thread(void) -> void {
@@ -32,8 +37,8 @@ auto test_gnet_thread(void) -> void {
 
 auto main(void) -> int {
   test_noncopyable();
-  test_vector();
-  test_string();
+  test_gnet_vector();
+  test_gnet_string();
   test_gnet_thread();
   return 0;
 }
