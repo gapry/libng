3cdf7fb6e03afaac6010c535b42f0d14a5ff9015
gapry <gapry@protonmail.com>
2021-03-19 17:00:01 +0800

    render: it needs to be noncopyable

--- src/client/render.h
+++ src/client/render.h
@@ -2,11 +2,12 @@
 #define __GNET_RENDER_H__
 
 #include "client/player.h"
+#include "noncopyable.h"
 #include "pch.h"
 
 namespace gnet {
 
-class render {
+class render : public gnet::noncopyable {
 public:
   static void game_loop(SDL_Window* window, ImGuiIO& io, gnet::player& player);
 };
