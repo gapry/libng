1bf152404f363fe48b6a1d0136120a6f9c488a80
gapry <gapry@protonmail.com>
2021-03-19 20:39:35 +0800

    client: add a player

--- src/client/client.h
+++ src/client/client.h
@@ -2,10 +2,13 @@
 #define __GNET_CLIENT_H__
 
 #include "client/game.h"
+#include "client/player.h"
 
 namespace gnet {
 
 class client : public gnet::game {
+  gnet::player player = gnet::player(400.0f, 300.0f);
+
 public:
   virtual void on_update(float delta_time) override final;
 
