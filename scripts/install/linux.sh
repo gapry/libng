59c0810e5094ed9dee3ed07a7307db5e95614b15
gapry <gapry@protonmail.com>
2021-03-20 00:43:20 +0800

    vcpkg: support default install in the Linux platform

---
+++ scripts/install/linux.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare -r dir_vcpkg=externals/vcpkg
+declare -r cmd_vcpkg=$dir_vcpkg/vcpkg
+
+git clone https://github.com/microsoft/vcpkg.git $dir_vcpkg
+
+bash -x $dir_vcpkg/bootstrap-vcpkg.sh
+
+$cmd_vcpkg install fmt --recurse
+$cmd_vcpkg install imgui[sdl2-binding] --recurse
+$cmd_vcpkg install imgui[opengl3-glew-binding] --recurse
