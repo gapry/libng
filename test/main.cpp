f3a7f239782be32e725d8efc82ac2fa342f7a24d
gapry <gapry@protonmail.com>
2021-04-02 23:27:30 +0800

    test: update

--- test/main.cpp
+++ test/main.cpp
@@ -1,27 +1,22 @@
 #include <fmt/core.h>
 
-#include "gnet/string.h"
-#include "gnet/vector.h"
+#include "gnet/string.hpp"
+#include "gnet/vector.hpp"
 
-#include "gstring.h"
-#include "gvector.h"
+#include "gstring.hpp"
+#include "gvector.hpp"
 
-#include "noncopyable.h"
-#include "thread/thread.h"
-
-auto test_noncopyable(void) -> void {
-  gnet::noncopyable obj;
-}
+#include "thread/thread.hpp"
 
 auto test_gnet_vector(void) -> void {
-  gnet::beta::vector<int> v1;
+  gnet::libcxx::vector<int> v1;
   gnet::vector<int> v2;
   v2.push_back(11);
   fmt::print("{}\n", v2[0]);
 }
 
 auto test_gnet_string(void) -> void {
-  gnet::beta::string msg1;
+  gnet::libcxx::string msg1;
   gnet::string msg2 = "I go to school by bus";
   fmt::print("{}\n", msg2);
 }
@@ -36,7 +31,6 @@ auto test_gnet_thread(void) -> void {
 }
 
 auto main(void) -> int {
-  test_noncopyable();
   test_gnet_vector();
   test_gnet_string();
   test_gnet_thread();
