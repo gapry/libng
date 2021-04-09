0e684c0d31bd067340e5c8a4d109e7f037cd040b
gapry <gapry@protonmail.com>
2021-04-09 20:50:03 +0800

    cmake: support the ProcessorCount

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -11,3 +11,7 @@ endif(MSVC)
 set(CMAKE_CXX_STANDARD          17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS        OFF)
+
+include(ProcessorCount)
+ProcessorCount(NPROC)
+set(CMAKE_CXX_FLAGS_INIT "-j${NPROC}")
