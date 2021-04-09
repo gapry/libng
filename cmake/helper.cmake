799a8e72ce42fdc28fa7eee882d622ac0e35c4b3
gapry <gapry@protonmail.com>
2021-04-09 13:20:53 +0800

    cmake: init the helper

---
+++ cmake/helper.cmake
@@ -0,0 +1,8 @@
+function(gnet_add_source path files)
+endfunction(gnet_add_source)
+
+function(gnet_add_library name path)
+endfunction(gnet_add_library)
+
+function(gnet_add_executable name path)
+endfunction(gnet_add_executable)
