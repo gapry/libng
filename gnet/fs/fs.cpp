1ca2867ee669ced1220f66aae53ed4034a360b4d
gapry <gapry@protonmail.com>
2021-04-13 11:36:43 +0800

    fs: fix the definition

--- gnet/fs/fs.cpp
+++ gnet/fs/fs.cpp
@@ -1,34 +1,38 @@
 #include "fs/fs.hpp"
 
-gnet::fs::~fs() {
+namespace gnet {
+
+fs::~fs() {
   this->close();
 }
 
 #ifdef GNET_OS_WINDOWS
-auto gnet::fs::open(void) -> void {
+auto fs::open(char const* const filename) -> void {
 }
 
-auto gnet::fs::read(void) -> void {
+auto fs::read(void) -> void {
 }
 
-auto gnet::fs::write(void) -> void {
+auto fs::write(void) -> void {
 }
 
-auto gnet::fs::close(void) -> void {
+auto fs::close(void) -> void {
 }
 #endif
 
 #if defined(GNET_OS_LINUX) || defined(GNET_OS_FREEBSD)
-auto gnet::fs::open(void) -> void {
+auto fs::open(char const* const filename) -> void {
 }
 
-auto gnet::fs::read(void) -> void {
+auto fs::read(void) -> void {
 }
 
-auto gnet::fs::write(void) -> void {
+auto fs::write(void) -> void {
 }
 
-auto gnet::fs::close(void) -> void {
+auto fs::close(void) -> void {
 }
 
 #endif
+
+} // namespace gnet
