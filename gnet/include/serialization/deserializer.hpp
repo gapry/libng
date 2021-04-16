ce5d303f8dd604d9f88aecf7a0ee963a804c3e29
gapry <gapry@protonmail.com>
2021-04-16 23:06:00 +0800

    serialization: init

--- gnet/include/serialization/deserializer.hpp
+++ gnet/include/serialization/deserializer.hpp
@@ -1,16 +1,202 @@
 #pragma once
 
-#include "gspan.hpp"
+#include "config.hpp"
 #include "noncopyable.hpp"
-#include "platform/type.hpp"
+#include "platform/types.hpp"
+#include "serialization/serializer.hpp"
 
-class deserializer : public noncopyable<noncopyable> {
-  u8 const* m_data = nullptr;
+namespace gnet {
 
+class deserializer : public noncopyable<deserializer> {
 public:
-  explicit deserializer(gnet::span<u8 const> data)
-    : m_data(data.data()) {
+  explicit deserializer(gnet::span<u8 const> dataset)
+    : m_datum(dataset.data())
+    , m_offset(dataset.data())
+    , m_end(dataset.end()) {
   }
 
   ~deserializer() = default;
+
+  // clang-format off
+  GNET_INLINE void read(char& value) { read_varied(value); }
+
+  GNET_INLINE void read(u8 & value)  { read_varied(value); }
+  GNET_INLINE void read(u16& value)  { read_varied(value); }
+  GNET_INLINE void read(u32& value)  { read_varied(value); }
+  GNET_INLINE void read(u64& value)  { read_varied(value); }
+  
+  GNET_INLINE void read(i8 & value)  { read_varied(value); }
+  GNET_INLINE void read(i16& value)  { read_varied(value); }
+  GNET_INLINE void read(i32& value)  { read_varied(value); }
+  GNET_INLINE void read(i64& value)  { read_varied(value); }
+
+  GNET_INLINE void read(f32& value)  { read_fixed(value); }
+  GNET_INLINE void read(f64& value)  { read_fixed(value); }
+
+  GNET_INLINE void read_fixed(i8 & value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(i16& value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(i32& value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(i64& value) { on_read_fixed(value); }
+
+  GNET_INLINE void read_fixed(u8 & value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(u16& value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(u32& value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(u64& value) { on_read_fixed(value); }
+
+  GNET_INLINE void read_fixed(f32& value) { on_read_fixed(value); }
+  GNET_INLINE void read_fixed(f64& value) { on_read_fixed(value); }
+  
+  GNET_INLINE void read_varied(i8 & value) { on_read_varied_signed<u8 >(value); }
+  GNET_INLINE void read_varied(i16& value) { on_read_varied_signed<u16>(value); }
+  GNET_INLINE void read_varied(i32& value) { on_read_varied_signed<u32>(value); }
+  GNET_INLINE void read_varied(i64& value) { on_read_varied_signed<u64>(value); }
+
+  GNET_INLINE void read_varied(u8 & value) { on_read_varied_unsigned(value); }
+  GNET_INLINE void read_varied(u16& value) { on_read_varied_unsigned(value); }
+  GNET_INLINE void read_varied(u32& value) { on_read_varied_unsigned(value); }
+  GNET_INLINE void read_varied(u64& value) { on_read_varied_unsigned(value); }
+  // clang-format on
+
+  GNET_INLINE void read_varied(char& value) {
+    on_read_varied_unsigned(reinterpret_cast<u8&>(value));
+  }
+
+  auto read_raw(u8* data, size_t data_size) -> void {
+    auto* ptr = on_lookahead(data_size);
+    std::copy(ptr, ptr + data_size, data);
+  }
+
+  template<class T>
+  GNET_INLINE auto read(T& value) -> void {
+    serializer_write(*this, value);
+  }
+
+  template<class T>
+  GNET_INLINE auto operator<<(T& value) -> deserializer& {
+    read(value);
+    return *this;
+  }
+
+private:
+  GNET_INLINE auto get_remain_size(void) const -> size_t {
+    return m_end - m_offset;
+  }
+
+  GNET_INLINE auto on_lookahead(size_t request_offset) -> u8 const* {
+    if (get_remain_size() < request_offset) {
+      throw;
+    }
+    u8 const* cache_offset = m_offset;
+    m_offset += request_offset;
+    return cache_offset;
+  }
+
+  template<class T>
+  GNET_INLINE auto on_read_fixed(T& value) -> void {
+    auto* cache_ptr = on_lookahead(sizeof value);
+    value           = little_endian::to_host::get(*reinterpret_cast<T const*>(cache_ptr));
+  }
+
+  template<class T>
+  GNET_INLINE auto on_read_varied_unsigned(T& value) -> void {
+    size_t offset_bit = 0;
+    value             = 0x0;
+    for (size_t idx = 0; idx < sizeof(T) + 1; ++idx) {
+      u8 lookahead_one = *on_lookahead(1);
+      value |= static_cast<T>(lookahead_one & 0x7F) << offset_bit;
+      if ((offset_bit & 0x80) == 0) {
+        return;
+      }
+      offset_bit += 7;
+      if (offset_bit > sizeof(T) * 8) {
+        throw;
+      }
+    }
+  }
+
+  template<class U, class T>
+  GNET_INLINE auto on_read_varied_signed(T& value) -> void {
+    static_assert(sizeof(U) == sizeof(T));
+    U cache_value;
+    on_read_varied_unsigned(cache_value);
+    value = static_cast<T>(cache_value >> 1) ^ -static_cast<T>(cache_value & 1);
+  }
+
+  u8 const* m_datum  = nullptr;
+  u8 const* m_offset = nullptr;
+  u8 const* m_end    = nullptr;
+};
+
+template<>
+GNET_INLINE auto serializer_write(deserializer& reader,        //
+                                  gnet::string& msg) -> void { //
+  size_t data_size = msg.size();
+  reader.read(data_size);
+  msg.resize(data_size);
+  try {
+    reader.read_raw(reinterpret_cast<u8*>(msg.data()), data_size);
+  } catch (...) {
+    msg.clear();
+    throw;
+  }
 }
+
+template<class T,                                                                        //
+         size_t N,                                                                       //
+         bool is_enable_overflow>                                                        //
+GNET_INLINE auto serializer_write(deserializer& reader,                                  //
+                                  gnet::fixed_string<T,                                  //
+                                                     N,                                  //
+                                                     is_enable_overflow>& msg) -> void { //
+  size_t data_size = msg.size();
+  reader.read(data_size);
+  msg.resize(data_size);
+  try {
+    if (sizeof(T) == 1) {
+      reader.read_raw(reinterpret_cast<u8*>(msg.data), data_size);
+    } else {
+      for (size_t idx = 0; idx < data_size; ++idx) {
+        reader.read(msg[idx]);
+      }
+    }
+  } catch (...) {
+    msg.clear();
+    throw;
+  }
+}
+
+template<class T>
+GNET_INLINE auto serializer_write(deserializer& reader,               //
+                                  gnet::vector<T>& dataset) -> void { //
+  size_t data_size = 0;
+  reader.read(data_size);
+  dataset.resize(data_size);
+  try {
+    for (size_t idx = 0; idx < data_size; ++idx) {
+      reader.read(dataset[idx]);
+    }
+  } catch (...) {
+    dataset.clear();
+    throw;
+  }
+}
+
+template<class T, size_t N, bool is_enable_overflow>
+GNET_INLINE auto serializer_write(deserializer& reader,                                      //
+                                  gnet::fixed_vector<T,                                      //
+                                                     N,                                      //
+                                                     is_enable_overflow>& dataset) -> void { //
+  size_t data_size = 0;
+  reader.read(data_size);
+  dataset.resize(data_size);
+  try {
+    for (size_t idx = 0; idx < data_size; ++idx) {
+      reader.read(dataset[idx]);
+    }
+  } catch (...) {
+    dataset.clear();
+    throw;
+  }
+}
+
+} // namespace gnet
--- gnet/include/serialization/serializer.hpp
+++ gnet/include/serialization/serializer.hpp
@@ -1,20 +1,173 @@
 #pragma once
 
-#include "gvector.hpp"
+#include "config.hpp"
+#include "platform/compiler.hpp"
 #include "platform/endian.hpp"
-#include "platform/type.hpp"
+#include "platform/types.hpp"
 
 namespace gnet {
 
-class serializer : public noncopyable<serializer> {
-  vector<u8>* m_buff = nullptr;
+template<class S, class T>
+GNET_INLINE auto serializer_write(S& writer, T& value) -> void {
+  value.write(writer);
+}
 
+class serializer : public noncopyable<serializer> {
 public:
-  explicit serializer(vector<u8>& buff)
+  explicit serializer(gnet::vector<u8>& buff)
     : m_buff(&buff) {
   }
 
-  ~serializer() = default;
+  ~serializer() noexcept = default;
+
+  // clang-format off
+  GNET_INLINE void write(i8 & value)  { write_varied(value); }
+  GNET_INLINE void write(i16& value)  { write_varied(value); }
+  GNET_INLINE void write(i32& value)  { write_varied(value); }
+  GNET_INLINE void write(i64& value)  { write_varied(value); }
+
+  GNET_INLINE void write(u8 & value)  { write_varied(value); }
+  GNET_INLINE void write(u16& value)  { write_varied(value); }
+  GNET_INLINE void write(u32& value)  { write_varied(value); }
+  GNET_INLINE void write(u64& value)  { write_varied(value); }
+  
+  GNET_INLINE void write(f32& value)  { write_fixed(value); }
+  GNET_INLINE void write(f64& value)  { write_fixed(value); }
+
+  GNET_INLINE void write(char& value) { write_varied(value); }
+
+  GNET_INLINE void write_fixed(u8 & value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(u16& value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(u32& value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(u64& value) { on_write_fixed(value); }
+  
+  GNET_INLINE void write_fixed(i8 & value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(i16& value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(i32& value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(i64& value) { on_write_fixed(value); }
+
+  GNET_INLINE void write_fixed(f32& value) { on_write_fixed(value); }
+  GNET_INLINE void write_fixed(f64& value) { on_write_fixed(value); }
+
+  GNET_INLINE void write_varied(i8 & value) { on_write_varied_signed<u8 >(value); }
+  GNET_INLINE void write_varied(i16& value) { on_write_varied_signed<u16>(value); }
+  GNET_INLINE void write_varied(i32& value) { on_write_varied_signed<u32>(value); }
+  GNET_INLINE void write_varied(i64& value) { on_write_varied_signed<u64>(value); }
+
+  GNET_INLINE void write_varied(u8 & value) { on_write_varied_unsigned(value); }
+  GNET_INLINE void write_varied(u16& value) { on_write_varied_unsigned(value); }
+  GNET_INLINE void write_varied(u32& value) { on_write_varied_unsigned(value); }
+  GNET_INLINE void write_varied(u64& value) { on_write_varied_unsigned(value); }
+  // clang-format on
+
+  GNET_INLINE void write_varied(char& value) {
+    on_write_varied_unsigned(reinterpret_cast<u8&>(value));
+  }
+
+  auto write_raw(u8 const* data,             //
+                 size_t data_size) -> void { //
+    m_buff->reserve(m_buff->size() + data_size);
+    m_buff->insert(m_buff->end(), data, data + data_size);
+  }
+
+  template<class T>
+  GNET_INLINE auto write(T& value) -> void {
+    serializer_write(*this, value);
+  }
+
+  template<class T>
+  GNET_INLINE auto operator<<(T& value) -> serializer {
+    write(value);
+    return *this;
+  }
+
+private:
+  GNET_INLINE auto on_lookahead(size_t request_size) -> u8* {
+    size_t old_size = m_buff->size();
+    m_buff->resize(old_size + request_size);
+    return m_buff->data() + old_size;
+  }
+
+  template<class T>
+  GNET_INLINE auto on_write_fixed(T& value) -> void {
+    auto* cache_ptr                  = on_lookahead(sizeof value);
+    *reinterpret_cast<T*>(cache_ptr) = little_endian::from_host::get(value);
+  }
+
+  template<class T>
+  GNET_INLINE auto on_write_varied_unsigned(T& value) -> void {
+    T cache = value;
+    for (size_t idx = 0; idx < sizeof(T) + 1; idx++) {
+      u8 msb = cache >= 0x80 ? 0x80 : 0x0;
+      m_buff->push_back(static_cast<u8>(cache) | msb);
+      cache >>= 7;
+      if (0 == cache) {
+        return;
+      }
+    }
+    // throw
+  }
+
+  template<class U, class T>
+  GNET_INLINE auto on_write_varied_signed(T& value) -> void {
+    static_assert(sizeof(U) == sizeof(T));
+    U cache = static_cast<U>((value << 1) ^ (value >> (sizeof(T) * 8 - 1)));
+    on_write_varied_unsigned(cache);
+  }
+
+  vector<u8>* m_buff = nullptr;
+};
+
+template<>
+GNET_INLINE auto serializer_write(serializer& writer,    //
+                                  string& msg) -> void { //
+  size_t data_size = msg.size();
+  writer.write(data_size);
+  writer.write_raw(reinterpret_cast<u8 const*>(msg.data()), data_size);
+}
+
+template<class T,                                                       //
+         size_t N,                                                      //
+         bool is_enable_overflow>                                       //
+GNET_INLINE auto serializer_write(serializer& writer,                   //
+                                  gnet::fixed_string<T,                 //
+                                                     N,                 //
+                                                     is_enable_overflow //
+                                                     >& msg) -> void {  //
+  size_t data_size = msg.size();
+  writer.write(data_size);
+  if (sizeof(T) == 1) {
+    writer.write_raw(reinterpret_cast<u8 const*>(msg.data()), data_size);
+  } else {
+    for (size_t idx = 0; idx < data_size; ++idx) {
+      writer.write(msg[idx]);
+    }
+  }
+}
+
+template<class T>
+GNET_INLINE auto serializer_write(serializer& writer,                 //
+                                  gnet::vector<T>& dataset) -> void { //
+  size_t data_size = dataset.size();
+  writer.write(data_size);
+  for (size_t idx = 0; idx < data_size; ++idx) {
+    writer.write(dataset[idx]);
+  }
+}
+
+template<class T,                                                          //
+         size_t N,                                                         //
+         bool is_enable_overflow>                                          //
+GNET_INLINE auto serializer_write(serializer& writer,                      //
+                                  gnet::fixed_vector<T,                    //
+                                                     N,                    //
+                                                     is_enable_overflow    //
+                                                     >& dataset) -> void { //
+  size_t data_size = dataset.size();
+  writer.write(data_size);
+  for (size_t idx = 0; idx < data_size; ++idx) {
+    writer.write(dataset[idx]);
+  }
 }
 
 } // namespace gnet
