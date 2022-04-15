4185247ed1335e891245ce3d37b9f362f32bb05c
gapry <gapry@protonmail.com>
2022-04-15 21:34:29 +0800

    cmake: config MSVC

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -4,8 +4,10 @@ set(CMAKE_CXX_COMPILER "clang++")
 endif(LINUX)
 
 if(MSVC)
-set(CMAKE_C_COMPILER   "cl")
-set(CMAKE_CXX_COMPILER "cl")
+set(CMAKE_C_COMPILER   "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.30.30705/bin/Hostx86/x86/cl.exe")
+set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.30.30705/bin/Hostx86/x86/cl.exe")
+set(CMAKE_CXX_FLAGS "/nologo /Zi /W4 /EHsc /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS")
+set(CMAKE_EXE_LINKER_FLAGS "/link /subsystem:console user32.lib gdi32.lib")
 endif(MSVC)
 
 set(CMAKE_CXX_STANDARD          17)
