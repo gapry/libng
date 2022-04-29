3086e66ad6fa707cd15e15b8d332f898a6f362e2
gapry <gapry@protonmail.com>
2022-04-29 16:06:37 +0800

    LinearAllocator: init

---
+++ libng/mm/LinearAllocator.cpp
@@ -0,0 +1,11 @@
+#include <mm/LinearAllocator.hpp>
+
+namespace libng {
+
+LinearAllocator::LinearAllocator() {
+}
+
+LinearAllocator::~LinearAllocator() {
+}
+
+} // namespace libng
---
+++ libng/mm/LinearAllocator.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace libng {
+
+class LinearAllocator {
+public:
+  LinearAllocator();
+
+  LinearAllocator();
+};
+
+} // namespace libng
