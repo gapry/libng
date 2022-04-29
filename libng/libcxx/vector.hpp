7718078ef5851fc07c6b9e8724a2d14fe4a1d3c5
gapry <gapry@protonmail.com>
2022-04-30 05:48:50 +0800

    libcxx: init vector

--- libng/libcxx/vector.hpp
+++ libng/libcxx/vector.hpp
@@ -1,21 +1,110 @@
 #pragma once
 
+#include <cassert>
+#include <new>
+#include <utility>
 #include <fmt/core.h>
+#include <types/noncopyable.hpp>
+#include <exception/error.hpp>
 
-#include "dbg.hpp"
+namespace libng {
 
-namespace libng::libcxx {
+namespace libcxx {
 
 template<typename T>
-class vector {
-public:
-  vector() {
-    fmt::print("{}\n", __LIBNG_PRETTY_FUNCTION__);
-  }
+class Vector : public NonCopyable {
+  using size_type = int;
 
-  ~vector() {
-    fmt::print("{}\n", __LIBNG_PRETTY_FUNCTION__);
-  }
+public:
+  Vector() = default;
+
+  ~Vector() noexcept;
+
+  T& operator[](int i);
+
+  const T& operator[](int i) const;
+
+  void resize(size_type new_size);
+
+  void reserve(size_type n);
+
+  size_type size(void) const;
+
+private:
+  T* m_data            = nullptr;
+  size_type m_size     = 0;
+  size_type m_capacity = 0;
 };
 
-} // namespace libng::libcxx
+template<typename T>
+Vector<T>::~Vector() noexcept {
+  if (m_data != nullptr) {
+    std::free(m_data);
+  }
+}
+
+template<typename T>
+T& Vector<T>::operator[](int i) {
+  return m_data[i];
+}
+
+template<typename T>
+const T& Vector<T>::operator[](int i) const {
+  return m_data[i];
+}
+
+template<typename T>
+void Vector<T>::resize(size_type new_size) {
+  if (m_size == new_size) {
+    return;
+  }
+  reserve(new_size);
+  if (new_size > m_size) {
+    auto* dst = m_data + m_size;
+    auto* end = m_data + new_size;
+    for (; dst < end; dst++) {
+      new (dst) T();
+    }
+  }
+  m_size = new_size;
+}
+
+template<typename T>
+void Vector<T>::reserve(size_type n) {
+  if (m_capacity >= n) {
+    return;
+  }
+
+  auto new_capacity = n;
+  auto new_data     = reinterpret_cast<T*>(std::malloc(new_capacity * sizeof(T)));
+
+  try {
+    auto* src = m_data;
+    auto* dst = new_data;
+    auto* end = m_data + m_size;
+    for (; src < end; src++, dst++) {
+      new (dst) T(std::move(*src));
+      src->~T();
+    }
+    if (m_data != nullptr) {
+      std::free(m_data);
+    }
+  } catch (...) {
+    // Issue: how to rewind
+    if (new_data != nullptr) {
+      std::free(new_data);
+    }
+    throw LIBNG_ERROR("Can't not reserve");
+  }
+  m_data     = new_data;
+  m_capacity = new_capacity;
+}
+
+template<typename T>
+Vector<T>::size_type Vector<T>::size(void) const {
+  return m_size;
+}
+
+} // namespace libcxx
+
+} // namespace libng
