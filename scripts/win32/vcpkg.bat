be56fedf06a59a276873b4fa612a89e223b8dd22
gapry <gapry@protonmail.com>
2022-05-06 05:32:32 +0800

    vcpkg: support x86 and x64

--- scripts/win32/vcpkg.bat
+++ scripts/win32/vcpkg.bat
@@ -10,9 +10,14 @@ git clone https://github.com/microsoft/vcpkg.git %dir_vcpkg%
 
 call %dir_vcpkg%\bootstrap-vcpkg.bat
 
-%dir_vcpkg%\vcpkg install fmt   --recurse
-%dir_vcpkg%\vcpkg install eastl --recurse
-%dir_vcpkg%\vcpkg install glew  --recurse
-%dir_vcpkg%\vcpkg install imgui --recurse
+%dir_vcpkg%\vcpkg install fmt:x86-windows   --recurse
+%dir_vcpkg%\vcpkg install eastl:x86-windows --recurse
+%dir_vcpkg%\vcpkg install glew:x86-windows  --recurse
+%dir_vcpkg%\vcpkg install imgui:x86-windows --recurse
+
+%dir_vcpkg%\vcpkg install fmt:x64-windows   --recurse
+%dir_vcpkg%\vcpkg install eastl:x64-windows --recurse
+%dir_vcpkg%\vcpkg install glew:x64-windows  --recurse
+%dir_vcpkg%\vcpkg install imgui:x64-windows --recurse
 
 @pause
