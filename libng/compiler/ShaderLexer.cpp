0eddaa11cb34b49af838c2f98d6552713e2e01e0
gapry <gapry@protonmail.com>
2022-06-03 21:17:57 +0800

    ShaderLexer: init the Token type

--- libng/compiler/ShaderLexer.cpp
+++ libng/compiler/ShaderLexer.cpp
@@ -2,41 +2,13 @@
 
 namespace libng {
 
-int ShaderLexer::lookahead(void) {
-  int chr = _getNextChar();
-  return chr;
+void ShaderLexer::Token::setNone() {
+  type = TokenType::None;
+  str.clear();
 }
 
-int ShaderLexer::skip(void) {
-  int chr = lookahead();
-
-  bool isSpace   = ' ' == chr;
-  bool isNewline = '\n' == chr;
-  bool isEnter   = '\r' == chr;
-  bool isFeed    = '\f' == chr;
-
-  bool isSkip = isSpace || isNewline || isEnter || isFeed;
-
-  while (isSkip) {
-    chr = lookahead();
-  }
-  return chr;
-}
-
-void ShaderLexer::execute(TokenType tokenType) {
-  // int chr = skip();
-  switch (tokenType) {
-    case TokenType::Identifier: break;
-    case TokenType::Number: break;
-    case TokenType::String: break;
-    case TokenType::Operator: break;
-    case TokenType::Newline: break;
-    default: break;
-  }
-}
-
-char ShaderLexer::_getNextChar(void) {
-  return ' ';
+void ShaderLexer::Token::onFormat(fmt::format_context& ctx) const {
+  // fmt::format_to(ctx.out(), "({}, {})", type, str); // Issue: need to fix
 }
 
 } // namespace libng
--- libng/compiler/ShaderLexer.hpp
+++ libng/compiler/ShaderLexer.hpp
@@ -1,29 +1,48 @@
 #pragma once
 
+#include <types/noncopyable.hpp>
+#include <libcxx/util/util.hpp>
+#include <libcxx/util/fmt.hpp>
+
 namespace libng {
 
-enum class ShaderLexerTokenType
-{
-  None,
-  Identifier,
-  Number,
-  String,
-  Operator,
-  Newline,
-};
-
-class ShaderLexer {
+class ShaderLexer : public NonCopyable {
 public:
-  using TokenType = ShaderLexerTokenType;
+  enum class TokenType
+  {
+    None,
+    Identifier,
+    Number,
+    String,
+    Operator,
+    Newline,
+  };
 
-  int lookahead(void);
+  struct Token {
+    TokenType type = TokenType::None;
+    String str;
 
-  int skip(void);
+    // clang-format off
+    bool isNone()                const { return TokenType::None == type; }
 
-  void execute(TokenType token);
+    bool isIdentifier()          const { return TokenType::Identifier == type; }
+    bool isIdentifier(StrView s) const { return TokenType::Identifier == type && s == str; }
 
-private:
-  char _getNextChar();
+    bool isNumber()              const { return TokenType::Number == type; }
+
+    bool isString()              const { return TokenType::String == type; }
+    bool isString(StrView s)     const { return TokenType::String == type && s == str; }
+
+    bool isOperator()            const { return TokenType::Operator == type;}
+    bool isOperator(StrView s)   const { return TokenType::String == type && s == str; }
+    
+    bool isNewLine()             const { return TokenType::Newline == type;}
+    // clang-format on
+
+    void setNone();
+
+    void onFormat(fmt::format_context& ctx) const;
+  };
 };
 
 } // namespace libng
