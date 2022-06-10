e860052a74881550f8de0da27e908be6a09414b2
gapry <gapry@protonmail.com>
2022-06-11 02:06:37 +0800

    scripts: adopt cmake with ninja

--- scripts/win32/unit_test.bat
+++ scripts/win32/unit_test.bat
@@ -1,6 +1,8 @@
 mkdir build
 cd build
-cmake -DBUILD_TEST=ON -G "NMake Makefiles" ..
-nmake 
+::cmake -DBUILD_TEST=ON -G "NMake Makefiles" ..
+::nmake 
+cmake -DBUILD_TEST=ON -GNinja ..
+ninja
 cd ..
 .\build\test\libng_test.exe
