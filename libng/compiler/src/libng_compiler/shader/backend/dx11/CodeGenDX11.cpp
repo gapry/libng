4247e9af87ec7815507141d5ab63a106f0dff85c
gapry <gapry@protonmail.com>
2022-06-24 21:21:39 +0800

    CodeGenDX11::_ReflectConstBuffers: init

--- libng/compiler/src/libng_compiler/shader/backend/dx11/CodeGenDX11.cpp
+++ libng/compiler/src/libng_compiler/shader/backend/dx11/CodeGenDX11.cpp
@@ -113,7 +113,7 @@ void CodeGenDX11::_ReflectInputs(ShaderStageInfo& outInfo,        //
   outInfo.inputs.reserve(desc.InputParameters);
 
   for (UINT i = 0; i < desc.InputParameters; i++) {
-    LIBNG_LOG("[Reflect Input] index = {}\n", i);
+    LIBNG_LOG("[ReflectInput] index = {}\n", i);
 
     D3D11_SIGNATURE_PARAMETER_DESC paramDesc;
     HRESULT hr = reflect->GetInputParameterDesc(i, &paramDesc);
@@ -155,10 +155,96 @@ void CodeGenDX11::_ReflectInputs(ShaderStageInfo& outInfo,        //
   }
 }
 
+// https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_shader_input_bind_desc
+// https://docs.microsoft.com/en-us/windows/win32/api/d3dcommon/ne-d3dcommon-d3d_shader_input_type
+//   - D3D_SIT_CBUFFER
+// https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_shader_variable_desc
+// https://docs.microsoft.com/en-us/windows/win32/api/d3d11shader/ns-d3d11shader-d3d11_shader_type_desc
+// https://docs.microsoft.com/en-us/windows/win32/api/d3dcommon/ne-d3dcommon-d3d_shader_variable_type
+//   - D3D_SVT_DOUBLE 
+// https://docs.microsoft.com/en-us/windows/win32/api/d3dcommon/ne-d3dcommon-d3d_shader_variable_flags
+//   - D3D_SVF_USED
 void CodeGenDX11::_ReflectConstBuffers(ShaderStageInfo& outInfo,        //
                                        ID3D11ShaderReflection* reflect, //
                                        D3D11_SHADER_DESC& desc) {       //
   LIBNG_LOG("CodeGen Stage = {}\n", __LIBNG_FUNCTION__);
+
+  outInfo.constBuffers.reserve(desc.BoundResources);
+
+  for (UINT i = 0; i < desc.BoundResources; i++) {
+    LIBNG_LOG("[ReflectConstBuffers] index = {}\n", i);
+
+    D3D11_SHADER_INPUT_BIND_DESC resDesc;
+
+    HRESULT hr = reflect->GetResourceBindingDesc(i, &resDesc);
+    Util::throwIfError(hr);
+
+    if (resDesc.Type != D3D_SIT_CBUFFER) {
+      continue;
+    }
+
+    D3D11_SHADER_BUFFER_DESC bufDesc;
+    auto cb = reflect->GetConstantBufferByName(resDesc.Name);
+    hr      = cb->GetDesc(&bufDesc);
+    Util::throwIfError(hr);
+
+    auto& outCB     = outInfo.constBuffers.emplace_back();
+    outCB.name      = bufDesc.Name;
+    outCB.bindPoint = static_cast<i16>(resDesc.BindPoint);
+    outCB.bindCount = static_cast<i16>(resDesc.BindCount);
+    outCB.dataSize  = bufDesc.Size;
+
+    outCB.variables.reserve(bufDesc.Variables);
+    {
+      for (UINT j = 0; j < bufDesc.Variables; j++) {
+        LIBNG_LOG("[ReflectConstBuffers] variable index = {}\n", j);
+
+        auto cbv = cb->GetVariableByIndex(j);
+        D3D11_SHADER_VARIABLE_DESC varDesc;
+        hr = cbv->GetDesc(&varDesc);
+        Util::throwIfError(hr);
+
+        D3D11_SHADER_TYPE_DESC varType;
+        hr = cbv->GetType()->GetDesc(&varType);
+        Util::throwIfError(hr);
+
+        if (0 == (varDesc.uFlags & D3D_SVF_USED)) {
+          continue;
+        }
+
+        auto& outVar  = outCB.variables.emplace_back();
+        outVar.name   = varDesc.Name;
+        outVar.offset = varDesc.StartOffset;
+
+        // clang-format off
+        TempString dataType;
+        switch (varType.Type) {
+          case D3D_SVT_BOOL   : dataType.append("Bool");    break;
+          case D3D_SVT_INT    : dataType.append("Int32");   break;
+          case D3D_SVT_UINT   : dataType.append("UInt32");  break;
+          case D3D_SVT_UINT8  : dataType.append("Uint8");   break;
+          case D3D_SVT_FLOAT  : dataType.append("Float32"); break;
+          case D3D_SVT_DOUBLE : dataType.append("Float64"); break;
+          default: throw LIBNG_ERROR("unsupported type {}", varType.Type);
+        }
+        switch (varType.Class) {
+          case D3D_SVC_SCALAR         : break;
+          case D3D_SVC_VECTOR         : FmtTo(dataType, "x{}",    varType.Columns); break;
+          case D3D_SVC_MATRIX_COLUMNS : FmtTo(dataType, "_{}x{}", varType.Rows, varType.Columns); break;
+          case D3D_SVC_MATRIX_ROWS    : FmtTo(dataType, "_{}x{}", varType.Rows, varType.Columns); break;
+          default: throw LIBNG_ERROR("unsupported Class {}", varType.Class);
+        }
+        // clang-format on
+
+        if (!enumTryParse(outVar.dataType, dataType)) {
+          throw LIBNG_ERROR("cannot parse dataType {}", dataType);
+        }
+        if (outVar.dataType == DataType::None) {
+          throw LIBNG_ERROR("dataType is None");
+        }
+      }
+    }
+  }
 }
 
 void CodeGenDX11::_ReflectTextures(ShaderStageInfo& outInfo,        //
