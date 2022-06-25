ea32dbc848c77800e3b95330b711541676450ca8
gapry <gapry@protonmail.com>
2022-06-25 17:00:58 +0800

    Vec2: update the tests

--- libng/core/src/libng_core/math/Vector/Vec2_Basic.hpp
+++ libng/core/src/libng_core/math/Vector/Vec2_Basic.hpp
@@ -1,7 +1,8 @@
-
 #pragma once
 
+#include <libng_core/exception/error.hpp>
 #include <libng_core/math/Tuple/Tuple2.hpp>
+#include <libng_core/types/function.hpp>
 
 namespace libng {
 
@@ -9,34 +10,49 @@ template<class T>
 using Vec2_Basic_Data = Tuple2<T>;
 
 template<class T, class DATA = Vec2_Basic_Data<T>>
-class Vec2_Basic : public DATA {
-  using Vec2                   = Vec2_Basic;
+struct Vec2_Basic : public DATA {
   static const size_t kElement = 2;
+
+  using Vec2        = Vec2_Basic;
+  using ElementType = T;
 
   using DATA::x;
   using DATA::y;
 
-public:
   // clang-format off
-  Vec2() = default;
-  Vec2(const DATA& v)                    { set(v); }
-  Vec2(const T& x_, const T& y_)         { set(x_, y_); }
+  LIBNG_INLINE          Vec2()                            { set(0, 0);   }
+  LIBNG_INLINE          Vec2(const DATA& v)               { set(v); }
+  LIBNG_INLINE          Vec2(const T& x_, const T& y_)    { set(x_, y_); }
 
-  void set(const DATA& v)                { DATA::set(v); }
-  void set(const T& x_, const T& y_)     { set(DATA(x_, y_)); }
+  LIBNG_INLINE     void set(const DATA& v)                { DATA::set(v); }
+  LIBNG_INLINE     void set(const T& x_, const T& y_)     { set(DATA(x_, y_)); }
 
-	Vec2 operator+(const Vec2& rhs) const  { return Vec2(x + rhs.x, y + rhs.y); }
-	Vec2 operator-(const Vec2& rhs) const  { return Vec2(x - rhs.x, y - rhs.y); }
-	Vec2 operator*(const Vec2& rhs) const  { return Vec2(x * rhs.x, y * rhs.y); }
-	Vec2 operator/(const Vec2& rhs) const  { return Vec2(x / rhs.x, y / rhs.y); }
+  LIBNG_INLINE const T& operator[](int i)           const { LIBNG_ASSERT(i >= 0);      return data[i]; }
+  LIBNG_INLINE       T& operator[](int i)                 { LIBNG_ASSERT(i >= 0);      return data[i]; }
 
-	Vec2 operator+(const T& s)      const  { return Vec2(x + s, y + s); }
-	Vec2 operator-(const T& s)      const  { return Vec2(x - s, y - s); }
-	Vec2 operator*(const T& s)      const  { return Vec2(x * s, y * s); }
-	Vec2 operator/(const T& s)      const  { return Vec2(x / s, y / s); }
+  LIBNG_INLINE     Vec2 operator+(const Vec2& rhs)  const {                            return Vec2(x + rhs.x, y + rhs.y); }
+  LIBNG_INLINE     Vec2 operator-(const Vec2& rhs)  const {                            return Vec2(x - rhs.x, y - rhs.y); }
+  LIBNG_INLINE     Vec2 operator*(const Vec2& rhs)  const {                            return Vec2(x * rhs.x, y * rhs.y); }
+  LIBNG_INLINE     Vec2 operator/(const Vec2& rhs)  const { LIBNG_ASSERT(rhs != 0);    return Vec2(x / rhs.x, y / rhs.y); }
 
-	bool operator==(const Vec2& rhs) const { return x == rhs.x && y == rhs.y; }
-	bool operator!=(const Vec2& rhs) const { return x != rhs.x || y != rhs.y; }
+  LIBNG_INLINE     Vec2 operator+(const T& scalar)  const {                            return Vec2(x + scalar, y + scalar); }
+  LIBNG_INLINE     Vec2 operator-(const T& scalar)  const {                            return Vec2(x - scalar, y - scalar); }
+  LIBNG_INLINE     Vec2 operator*(const T& scalar)  const {                            return Vec2(x * scalar, y * scalar); }
+  LIBNG_INLINE     Vec2 operator/(const T& scalar)  const { LIBNG_ASSERT(scalar != 0); return Vec2(x / scalar, y / scalar); }
+
+  LIBNG_INLINE     void operator+=(const Vec2& rhs)       {                            x += rhs.x; y += rhs.y; }
+  LIBNG_INLINE     void operator-=(const Vec2& rhs)       {                            x -= rhs.x; y -= rhs.y; }
+  LIBNG_INLINE     void operator*=(const Vec2& rhs)       {                            x *= rhs.x; y *= rhs.y; }
+  LIBNG_INLINE     void operator/=(const Vec2& rhs)       { LIBNG_ASSERT(rhs != 0);    x /= rhs.x; y /= rhs.y; }
+
+  LIBNG_INLINE     void operator+=(const T& scalar)       {                            x += scalar; y += scalar; }
+  LIBNG_INLINE     void operator-=(const T& scalar)       {                            x -= scalar; y -= scalar; }
+  LIBNG_INLINE     void operator*=(const T& scalar)       {                            x *= scalar; y *= scalar; }
+  LIBNG_INLINE     void operator/=(const T& scalar)       { LIBNG_ASSERT(scalar != 0); x /= scalar; y /= scalar; }
+  
+  LIBNG_INLINE     bool operator==(const Vec2& rhs) const { return x == rhs.x  && y == rhs.y;  }
+  LIBNG_INLINE     bool operator!=(const Vec2& rhs) const { return x != rhs.x  || y != rhs.y;  }
+  LIBNG_INLINE     bool operator!=(const T& scalar) const { return x != scalar || y != scalar; }
   // clang-format on
 
   void onFormat(fmt::format_context& ctx) const {
--- libng/core/test/src/libng_core_test/math/Vector/TestVec2.cpp
+++ libng/core/test/src/libng_core_test/math/Vector/TestVec2.cpp
@@ -23,15 +23,69 @@ public:
     LIBNG_DUMP_VAR(va);
     LIBNG_DUMP_VAR(vb);
 
-    Vec2d v1 = va + vb; // Issue
-    Vec2d v2 = va - vb; // Issue
-    Vec2d v3 = va * vb;
-    Vec2d v4 = va / vb;
+    {
+      Vec2d v1 = va + vb; // Issue
+      Vec2d v2 = va - vb; // Issue
+      Vec2d v3 = va * vb;
+      Vec2d v4 = va / vb;
 
-    LIBNG_DUMP_VAR(v1);
-    LIBNG_DUMP_VAR(v2);
-    LIBNG_DUMP_VAR(v3);
-    LIBNG_DUMP_VAR(v4);
+      LIBNG_DUMP_VAR(v1);
+      LIBNG_DUMP_VAR(v2);
+      LIBNG_DUMP_VAR(v3);
+      LIBNG_DUMP_VAR(v4);
+    }
+
+    {
+      Vec2d v1 = va + max; // Issue
+      Vec2d v2 = va - max; // Issue
+      Vec2d v3 = va * max;
+      Vec2d v4 = va / max;
+
+      LIBNG_DUMP_VAR(v1);
+      LIBNG_DUMP_VAR(v2);
+      LIBNG_DUMP_VAR(v3);
+      LIBNG_DUMP_VAR(v4);
+    }
+
+    {
+      Vec2d v1;
+      Vec2d v2;
+      Vec2d v3;
+      Vec2d v4;
+
+      v1 += va;
+      v2 -= va;
+      v3 *= va;
+      v4 /= va;
+
+      LIBNG_DUMP_VAR(v1);
+      LIBNG_DUMP_VAR(v2);
+      LIBNG_DUMP_VAR(v3);
+      LIBNG_DUMP_VAR(v4);
+    }
+
+    {
+      Vec2d v1;
+      Vec2d v2;
+      Vec2d v3;
+      Vec2d v4;
+
+      v1 += min;
+      v2 -= min;
+      v3 *= min;
+      v4 /= min;
+
+      LIBNG_DUMP_VAR(v1);
+      LIBNG_DUMP_VAR(v2);
+      LIBNG_DUMP_VAR(v3);
+      LIBNG_DUMP_VAR(v4);
+    }
+
+    {
+      Vec2d v(max, min);
+      LIBNG_DUMP_VAR(v[0]);
+      LIBNG_DUMP_VAR(v[1]);
+    }
   }
 };
 
