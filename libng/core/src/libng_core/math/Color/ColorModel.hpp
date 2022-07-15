2fb723301da4f3b8add7961f61df65b43cdffc1e
gapry <gapry@protonmail.com>
2022-07-15 18:49:37 +0800

    color: add ColorModel

---
+++ libng/core/src/libng_core/math/Color/ColorModel.hpp
@@ -0,0 +1,19 @@
+#include <libng_core/types/enum.hpp>
+#include <libng_core/types/number.hpp>
+
+namespace libng::math {
+
+#define ColorModel_ENUM_LIST(E) \
+  E(None, )                     \
+  E(R, )                        \
+  E(RG, )                       \
+  E(RGB, )                      \
+  E(RGBA, )                     \
+  E(L, )  /* Luminance */       \
+  E(LA, ) /* Luminance Alpha */ \
+  E(HSV, )                      \
+  E(BlockCompression, )         \
+//----
+LIBNG_ENUM_CLASS(ColorModel, u8)
+
+} // namespace libng::math
