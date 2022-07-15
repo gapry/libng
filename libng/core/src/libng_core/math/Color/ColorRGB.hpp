3a5b378b8d1fc04dbf793005c1b445ae3dc2d7ff
gapry <gapry@protonmail.com>
2022-07-15 19:57:02 +0800

    ColorRGB: update

--- libng/core/src/libng_core/math/Color/ColorRGB.hpp
+++ libng/core/src/libng_core/math/Color/ColorRGB.hpp
@@ -1,18 +1,27 @@
 #pragma once
 
+#include <libng_core/math/Color/ColorElementType.hpp>
+#include <libng_core/math/Color/ColorModel.hpp>
+#include <libng_core/math/Color/ColorType.hpp>
+
 namespace libng::math {
 
 template<class T>
 struct ColorRGB {
-  static const size_t kElement = 3;
+  using ElementType                 = T;
+  static const size_t kElementCount = 3;
+  static constexpr int kAlphaBits   = 0;
+  static constexpr ColorType kColorType =
+    libng::ColorType_make(libng::ColorModel::RGB, libng::ColorElementType_get<T>());
 
   union {
     struct {
       T r, g, b;
     };
-    T data[kElement];
+    T data[kElementCount];
   };
 
+  ColorRGB() = default;
   ColorRGB(const T& r_, const T& g_, const T& b_)
     : r(r_)
     , g(g_)
