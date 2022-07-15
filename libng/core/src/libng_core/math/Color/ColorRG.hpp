7f9491cbd89f23dba993ab1152994855fc2c2648
gapry <gapry@protonmail.com>
2022-07-15 19:44:52 +0800

    ColorRG: update

--- libng/core/src/libng_core/math/Color/ColorRG.hpp
+++ libng/core/src/libng_core/math/Color/ColorRG.hpp
@@ -1,17 +1,26 @@
 #pragma once
 
+#include <libng_core/math/Color/ColorElementType.hpp>
+#include <libng_core/math/Color/ColorModel.hpp>
+#include <libng_core/math/Color/ColorType.hpp>
+
 namespace libng::math {
 
 template<class T>
 struct ColorRG {
-  static const size_t kElement = 2;
+  using ElementType                     = T;
+  static const size_t kElementCount     = 2;
+  static constexpr int kAlphaBits       = 0;
+  static constexpr ColorType kColorType = ColorType_make(ColorModel::RG, ColorElementType_get<T>());
 
   union {
     struct {
       T r, g;
     };
-    T data[kElement];
+    T data[kElementCount];
   };
+
+  ColorRG() = default;
 
   ColorRG(const T& r_, const T& g_)
     : r(r_)
