5fa49d744bb47218d5bd323b8c54f18daf27d978
gapry <gapry@protonmail.com>
2022-07-22 00:08:14 +0800

    ShaderPropType: utilize macros to define

--- libng/renderer/src/libng_render/material/ShaderPropType.hpp
+++ libng/renderer/src/libng_render/material/ShaderPropType.hpp
@@ -4,26 +4,17 @@
 
 namespace libng {
 
-enum class ShaderPropType
-{
-  None,
-  Int,
-  Float,
-  Vec2f,
-  Vec3f,
-  Vec4f,
-  Color4f,
-};
-
 #define ShaderPropType_ENUM_LIST(E) \
-  E(Int)                            \
-  E(Float)                          \
-  E(Vec2f)                          \
-  E(Vec3f)                          \
-  E(Vec4f)                          \
-  E(Color4f)                        \
+  E(None, )                         \
+  E(Int, )                          \
+  E(Float, )                        \
+  E(Vec2f, )                        \
+  E(Vec3f, )                        \
+  E(Vec4f, )                        \
+  E(Color4f, )                      \
+  E(Texture2d, )                    \
   // ----
 
-LIBNG_ENUM_STR_UTIL(ShaderPropType)
+LIBNG_ENUM_CLASS(ShaderPropType, u8)
 
 } // namespace libng
