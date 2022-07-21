68ad64a0b41e4e59b942fd76dffe53b84e8ef310
gapry <gapry@protonmail.com>
2022-07-22 00:31:47 +0800

    ShaderPropValueConstPtr: update

--- libng/renderer/src/libng_render/material/ShaderPropValueConstPtr.hpp
+++ libng/renderer/src/libng_render/material/ShaderPropValueConstPtr.hpp
@@ -1,7 +1,34 @@
 #pragma once
 
+#include <libng_render/RendererCommon.hpp>
+#include <libng_render/material/ShaderPropType.hpp>
+#include <libng_render/material/ShaderPropTypeUtil.hpp>
+
 namespace libng {
 
-struct ShaderPropValueConstPtr {};
+struct ShaderPropValueConstPtr {
+  using Type = ShaderPropType;
+
+  Type type        = Type::None;
+  const void* data = nullptr;
+  size_t dataSize  = 0;
+
+  ShaderPropValueConstPtr() = default;
+
+  template<class V>
+  ShaderPropValueConstPtr(const V& val) {
+    type     = ShaderPropTypeUtil::get<V>();
+    data     = &val;
+    dataSize = sizeof(val);
+  }
+
+  template<class V>
+  const V& asValue() {
+    if (type != ShaderPropTypeUtil::get<V>()) {
+      throw LIBNG_ERROR("{}\n", "Invalid ShaderPropType");
+    }
+    return *reinterpret_cast<const V*>(data);
+  }
+};
 
 } // namespace libng
