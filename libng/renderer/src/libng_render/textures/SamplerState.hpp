6be5658b3edea2508ae26549fd95c7180cb09458
gapry <gapry@protonmail.com>
2022-07-22 19:22:06 +0800

    SampleState: update

--- libng/renderer/src/libng_render/textures/SamplerState.hpp
+++ libng/renderer/src/libng_render/textures/SamplerState.hpp
@@ -1,12 +1,26 @@
 #pragma once
 
 #include <libng_render/RendererCommon.hpp>
+#include <libng_render/textures/TextureFilter.hpp>
+#include <libng_render/textures/TextureWrap.hpp>
 
 namespace libng {
 
 struct SamplerState {
   SamplerState();
   ~SamplerState();
+
+  using Filter = TextureFilter;
+  using Wrap   = TextureWrap;
+
+  Filter filter = Filter::Bilinear;
+  Wrap wrapU    = Wrap::Repeat;
+  Wrap wrapV    = Wrap::Repeat;
+  Wrap wrapW    = Wrap::Repeat;
+
+  // LOD := Level of Detail
+  float minLOD = 0;
+  float maxLOD = math::inf<float>();
 };
 
 } // namespace libng
