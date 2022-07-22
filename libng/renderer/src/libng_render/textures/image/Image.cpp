303e5a814b97c2447606bcf663ee8b3957676190
gapry <gapry@protonmail.com>
2022-07-22 18:02:50 +0800

    Image: update

--- libng/renderer/src/libng_render/textures/image/Image.cpp
+++ libng/renderer/src/libng_render/textures/image/Image.cpp
@@ -6,8 +6,79 @@ Image::Image() {
   LIBNG_LOG("{}\n", __LIBNG_PRETTY_FUNCTION__);
 }
 
+Image::Image(Image&& rhs) {
+  LIBNG_LOG("{}\n", __LIBNG_PRETTY_FUNCTION__);
+}
+
 Image::~Image() {
   LIBNG_LOG("{}\n", __LIBNG_PRETTY_FUNCTION__);
+}
+
+void Image::operator=(Image&& rhs) {
+  LIBNG_LOG("{}\n", __LIBNG_PRETTY_FUNCTION__);
+}
+
+void Image::clear() {
+  _info = ImageInfo();
+  _pixelData.clear();
+}
+
+void Image::loadFile(StrView filename) {
+}
+
+void Image::loadPngFile(StrView filename) {
+  MemMapFile mm;
+  mm.open(filename);
+  loadPngMem(mm);
+}
+
+void Image::loadPngMem(ByteSpan data) {
+}
+
+void Image::loadDdsFile(StrView filename) {
+  MemMapFile mm;
+  mm.open(filename);
+  loadDdsMem(mm);
+}
+
+void Image::loadDdsMem(ByteSpan data) {
+}
+
+void Image::create(ColorType colorType, int width, int height) {
+  create(colorType, width, height, width * ColorUtil::pixelSizeInBytes(colorType));
+}
+
+void Image::create(ColorType colorType, int width, int height, int strideInBytes) {
+  _create(colorType, width, height, strideInBytes, 1, strideInBytes * height);
+}
+
+void Image::create(ColorType colorType,
+                   int width,
+                   int height,
+                   int strideInBytes,
+                   int mipmapCount,
+                   size_t dataSizeInBytes) {
+  _create(colorType, width, height, strideInBytes, mipmapCount, dataSizeInBytes);
+}
+
+void Image::_create(ColorType colorType,
+                    int width,
+                    int height,
+                    int strideInBytes,
+                    int mipmapCount,
+                    size_t dataSizeInBytes) {
+  _info.colorType = colorType;
+  _info.size.set(width, height);
+  _info.strideInBytes = strideInBytes;
+  _info.mipmapCount   = mipmapCount;
+
+  try {
+    _pixelData.clear();
+    _pixelData.resize(dataSizeInBytes);
+  } catch (...) {
+    clear();
+    throw;
+  }
 }
 
 } // namespace libng
--- libng/renderer/src/libng_render/textures/image/Image.hpp
+++ libng/renderer/src/libng_render/textures/image/Image.hpp
@@ -5,15 +5,94 @@
 
 namespace libng {
 
+// clang-format off
 class Image : public NonCopyable {
+  using Vec2i     = math::Vec2i;
+  using ColorType = math::ColorType;
+  using ColorUtil = math::ColorUtil;
+
 public:
   using Info = ImageInfo;
 
   Image();
+  Image(Image&& r);
+
   ~Image();
 
+  void operator=(Image&& r);
+
+  void clear();
+
+  void loadFile(StrView filename);
+
+  void loadPngFile(StrView filename);
+  void loadPngMem (ByteSpan data);
+
+  void loadDdsFile(StrView filename);
+  void loadDdsMem (ByteSpan data);
+
+  void create(ColorType colorType, int width, int height);
+  void create(ColorType colorType, int width, int height, int strideInBytes);
+  void create(ColorType colorType, int width, int height, int strideInBytes, int mipmapCount, size_t dataSizeInBytes);
+
+  LIBNG_INLINE const Info&  info()          const { return _info;               }
+  LIBNG_INLINE const Vec2i& size()          const { return _info.size;          }
+  LIBNG_INLINE int          strideInBytes() const { return _info.strideInBytes; }
+  LIBNG_INLINE int          width()         const { return _info.size.x;        }
+  LIBNG_INLINE int          height()        const { return _info.size.y;        }
+  LIBNG_INLINE ColorType    colorType()     const { return _info.colorType;     }
+
+  template<class COLOR> LIBNG_INLINE const COLOR& pixel(int x, int y) const { return row<COLOR>(y)[x]; }
+  template<class COLOR> LIBNG_INLINE       COLOR& pixel(int x, int y)       { return row<COLOR>(y)[x]; }
+
+  template<class COLOR> LIBNG_INLINE Span<      COLOR> row(int y)       { _checkType(COLOR::kColorType); return row_noCheck<COLOR>(y); }
+  template<class COLOR> LIBNG_INLINE Span<const COLOR> row(int y) const { _checkType(COLOR::kColorType); return row_noCheck<COLOR>(y); }
+
+  template<class COLOR> LIBNG_INLINE Span<      COLOR> row_noCheck(int y)       { return Span<      COLOR>(reinterpret_cast<      COLOR*>(rowBytes(y).data()), _info.size.x); }
+  template<class COLOR> LIBNG_INLINE Span<const COLOR> row_noCheck(int y) const { return Span<const COLOR>(reinterpret_cast<const COLOR*>(rowBytes(y).data()), _info.size.x); }
+
+  LIBNG_INLINE Span<      u8> rowBytes(int y)       { return Span<      u8>(&_pixelData[y * _info.strideInBytes], _info.size.x * _info.pixelSizeInBytes()); }
+  LIBNG_INLINE Span<const u8> rowBytes(int y) const { return Span<const u8>(&_pixelData[y * _info.strideInBytes], _info.size.x * _info.pixelSizeInBytes()); }
+
+  const void* dataPtr() const {
+    return _pixelData.data();
+  }
+
+  void copyToPixelData(ByteSpan src) {
+    _pixelData.assign(src.begin(), src.end());
+  }
+
+  template<class COLOR>
+  void fill(const COLOR& color);
+
 private:
+  void _create(ColorType colorType,     
+               int       width,               
+               int       height,              
+               int       strideInBytes,       
+               int       mipmapCount,         
+               size_t    dataSizeInBytes); 
+
+  void _checkType(ColorType colorType) const {
+    if (colorType != _info.colorType) {
+      throw LIBNG_ERROR("{}\n", "Invalid ColorType");
+    }
+  }
+
   Info _info;
+  Vector<u8> _pixelData;
 };
+
+// clang-format on
+
+template<class COLOR>
+LIBNG_INLINE void Image::fill(const COLOR& color) {
+  _checkType(COLOR::kColorType);
+  for (int y = 0; y < _info.size.y; y++) {
+    for (auto& p : row_noCheck<COLOR>(y)) {
+      p = color;
+    }
+  }
+}
 
 } // namespace libng
